0d180fb05bd68446859cfce9b1b89f7c797fb4b6
T.T.D <41204206+pappy0157@users.noreply.github.com>
2026-03-08 23:36:10 +0900

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,24 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        fs: false,
+        net: false,
+        tls: false,
+        crypto: false,
+        path: false,
+        os: false,
+        stream: false,
+        http: false,
+        https: false,
+        zlib: false,
+      };
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
